100c69cff342274d0d5f0c51d1afdcd53f116db9
JamesXChang <a0930748797@gmail.com>
2019-10-28 02:31:56 +0800

    localhost version

---
+++ src/storehash.js
@@ -0,0 +1,35 @@
+import web3 from './web3';
+
+var address = '0xF3D694B1cc97d72f095FAE628f7AbD1aFFB68B9D';
+
+var abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"name": "setHash",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getHash",
+		"outputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+export default new web3.eth.Contract(abi, address);
